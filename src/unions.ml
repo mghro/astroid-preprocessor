a83591fe37c2c00947dc175368210715ed1673f3
Thomas Madden <tmadden@gmail.com>
2023-08-15 21:53:12 -0400

    Add to_dynamic qualifiers

--- src/unions.ml
+++ src/unions.ml
@@ -416,7 +416,7 @@ let union_conversion_definitions u =
            ^ "\")); " ^ "x->contents_ = tmp; " ^ "break; " ^ " } ")
          u.union_members)
   ^ "} " ^ "} " ^ "std::ostream& operator<<(std::ostream& s, " ^ u.union_id
-  ^ " const& x) " ^ "{ return s << to_dynamic(x); } "
+  ^ " const& x) " ^ "{ return s << cradle::to_dynamic(x); } "
 
 let union_swap_declaration u =
   "void swap(" ^ u.union_id ^ "& a, " ^ u.union_id ^ "& b); "
